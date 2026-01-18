d61fec51f9f12bacf0b6cc9fe164d0e3cbc936ec
lizkisssss <158608014+lizkisssss@users.noreply.github.com>
2026-01-18 09:07:57 +0000

    Deploying to gh-pages from @ RomanPoliuga/rick-morty-project@9b0548a34e5b64c72f70bbd46d456aac8b3ab182 🚀

---
+++ assets/episodes-fdb8f4d1.js
@@ -0,0 +1,6 @@
+import"./main-4024f8b9.js";const p=document.querySelector("#season"),t=document.querySelector("#episodes"),d=document.querySelector(".seasons-list-btn");let r=[],i=[],u=10,n=0;p.addEventListener("change",m);d.addEventListener("click",l);h();async function h(){t.innerHTML="<p>Loading...</p>";try{let s="https://rickandmortyapi.com/api/episode",e=[];for(;s;){const a=await(await fetch(s)).json();e=e.concat(a.results),s=a.info.next}r=e,i=r,n=0,t.innerHTML="",l()}catch(s){console.error("Error fetching episodes:",s),t.innerHTML="<p>Failed to load episodes</p>"}}function m(s){const e=s.target.value;e==="all"?i=r:i=r.filter(o=>o.episode.startsWith(e)),n=0,t.innerHTML="",l()}function l(){const s=n+u,e=i.slice(n,s);f(e),n=s,n>=i.length?d.style.display="none":d.style.display="block"}function f(s){s.forEach(e=>{const o=e.episode.slice(1,3),a=`Season ${Number(o)}`,c=document.createElement("div");c.classList.add("episode-card"),c.innerHTML=`
+            <img src="images-seasons-list/season${o}.jpg" alt="${e.name}">
+            <h3>${e.name}</h3>
+            <p>${a}</p>
+            <p>${e.air_date}</p>
+        `,t.appendChild(c)})}
